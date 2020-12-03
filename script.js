66d85f3dfa0119043eabe3f4293dc4e05154d556
Henry <bloodofangel007@gmail.com>
2020-12-03 10:33:48 -0500

    ultimo cambio

--- script.js
+++ script.js
@@ -14,6 +14,35 @@ inicio("Pablito", function(valor) {
 
 //EJEMPLO DE PROMISES
 
+var promesa1 = function() {
+    return new Promise(function(resolver, cancelar) {
+
+        setTimeout(function() {
+            console.log("pasan 4 segundos");
+            resolver();
+
+        }, 4000);
+    });
+}
+
+
+
+var promesa2 = function() {
+    return new Promise(function(resolver, cancelar) {
+
+        setTimeout(function() {
+
+            console.log("pasan 1 segundos");
+            resolver();
+        }, 1000);
+
+    });
+}
+
+// promesa1().then(promesa2).then(function() {
+
+//     console.log("termino");
+// });
 
 
 
